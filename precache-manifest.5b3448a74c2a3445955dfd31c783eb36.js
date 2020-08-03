31f49ab9100c67455881562e9bb74953adc558dc
Pau Bonilla <paulo111995@gmail.com>
2020-08-02 20:43:34 -0700

    Updates

---
+++ precache-manifest.5b3448a74c2a3445955dfd31c783eb36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b285607bf70d46257f0c69acae0a5e",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "7567d3b84d31e4cb5767",
+    "url": "/tic-tac-toe/static/css/main.4c1bc847.chunk.css"
+  },
+  {
+    "revision": "ff3d1ec8fb9b5ca8db0f",
+    "url": "/tic-tac-toe/static/js/2.0034afb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tic-tac-toe/static/js/2.0034afb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7567d3b84d31e4cb5767",
+    "url": "/tic-tac-toe/static/js/main.3044b89c.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  },
+  {
+    "revision": "b09e93bf54cc2d253f6edebbfbc6655b",
+    "url": "/tic-tac-toe/static/media/michael-scott.b09e93bf.png"
+  },
+  {
+    "revision": "45ddc09554f9172f949df96a18ac8b39",
+    "url": "/tic-tac-toe/static/media/the-office-theme-song.45ddc095.mp3"
+  }
+]);
