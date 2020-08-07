d58ae4015168d121761fc8529dc1a69524c7aa77
Pau Bonilla <paulo111995@gmail.com>
2020-08-06 20:33:31 -0700

    Updates

--- precache-manifest.595dc6397e04d7dfc38e07a899d3afe1.js
+++ precache-manifest.595dc6397e04d7dfc38e07a899d3afe1.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7f95f79b3c954ca0cad94c5b05cd777b",
+    "revision": "aa667d595718af60a7e0d5614ec4a49c",
     "url": "/tic-tac-toe/index.html"
   },
   {
-    "revision": "d550996492bbafaa1ae0",
+    "revision": "fcaa261e0f57211f4390",
     "url": "/tic-tac-toe/static/css/main.1882bdc6.chunk.css"
   },
   {
@@ -16,16 +16,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tic-tac-toe/static/js/2.0034afb0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d550996492bbafaa1ae0",
-    "url": "/tic-tac-toe/static/js/main.a3146236.chunk.js"
+    "revision": "fcaa261e0f57211f4390",
+    "url": "/tic-tac-toe/static/js/main.e9d7b06a.chunk.js"
   },
   {
     "revision": "7f32745e7b69030faee7",
     "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
   },
   {
-    "revision": "b09e93bf54cc2d253f6edebbfbc6655b",
-    "url": "/tic-tac-toe/static/media/michael-scott.b09e93bf.png"
+    "revision": "0666a9a4d32eba9716a42e66672589c1",
+    "url": "/tic-tac-toe/static/media/michael-scott.0666a9a4.png"
   },
   {
     "revision": "45ddc09554f9172f949df96a18ac8b39",
